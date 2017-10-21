9d30fc7412b9e705d6756a06aeec682c40b46a8b
morzack <john_kesler@caryacademy.org>
2017-10-21 13:15:23 -0400

    Cleaned up the autonomous class

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/opmodes/AutonomousMain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/opmodes/AutonomousMain.java
@@ -1,52 +1,47 @@
 
 package org.firstinspires.ftc.teamcode.autonomous.opmodes;
 
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.util.ElapsedTime;
-import com.qualcomm.robotcore.util.Range;
 import com.qualcomm.robotcore.eventloop.opmode.*;
-
-
-
-import static com.sun.tools.javac.main.Option.S;
 
 @Autonomous(name="MainAutonomous")
 public class AutonomousMain extends LinearOpMode {
 
     // Declare OpMode members.
     private ElapsedTime runtime = new ElapsedTime();
-    private DcMotor FL = null;
-    private DcMotor FR = null;
-    private DcMotor RL = null;
-    private DcMotor RR = null;
-    private DcMotor L = null;
+    private DcMotor frontLeftMotor = null;
+    private DcMotor frontRightMotor = null;
+    private DcMotor rearLeftMotor = null;
+    private DcMotor rearRightMotor = null;
+    private DcMotor liftMotor = null;
 
 
 
     @Override
     public void runOpMode() {
-        //Initialize Telemetry
+        //Motor Directions
+
+
+        //Assign the motors to classes
+        frontLeftMotor = hardwareMap.get(DcMotor.class, "frontLeftMotor Drive");
+        frontRightMotor = hardwareMap.get(DcMotor.class, "frontRightMotor Drive");
+        rearLeftMotor = hardwareMap.get(DcMotor.class, "BL Drive");
+        rearRightMotor = hardwareMap.get(DcMotor.class, "BR Drive");
+        liftMotor = hardwareMap.get(DcMotor.class, "Riser Lift");
+
+        //Bind motor positions to different motors
+
+        //Let user know that robot has been initialized
         telemetry.addData("Status", "Initialized");
         telemetry.update();
-
-        //Motor Initialzation
-        FL = hardwareMap.get(DcMotor.class, "FL Drive");
-        FR = hardwareMap.get(DcMotor.class, "FR Drive");
-        RL = hardwareMap.get(DcMotor.class, "BL Drive");
-        RR = hardwareMap.get(DcMotor.class, "BR Drive");
-        L = hardwareMap.get(DcMotor.class, "Riser Lift");
-
-        //Motor Directions
 
         //Wait For Play, Reset Timer
         waitForStart();
         runtime.reset();
 
-        //Run until stop
+        //Run until stopped
         while(opModeIsActive()){
 
         }
