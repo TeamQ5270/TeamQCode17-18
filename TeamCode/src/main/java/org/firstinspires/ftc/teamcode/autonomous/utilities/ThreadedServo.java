b71fddf1f12f21a730ce0ff3bb5643cdac6b33ca
morzack <john_kesler@caryacademy.org>
2017-10-25 10:54:58 -0400

    Added in a class for doing servo operations on a different thread

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/utilities/ThreadedServo.java
@@ -0,0 +1,49 @@
+package org.firstinspires.ftc.teamcode.autonomous.utilities;
+
+import com.qualcomm.robotcore.hardware.Servo;
+
+public class ThreadedServo extends Thread{
+    //TODO Multithreading for the servo movement
+    public enum ServoPosition {IN, OUT}
+
+    private static double[] positionsOutLocal;
+    private static double[] positionsInLocal;
+
+    //Flip the servo objects based on inputted distances
+    public static ServoPosition flipServos(Servo[] servos, double[] positionsOut, double[] positionsIn, ServoPosition previousStatus) {
+        //For each servo
+        for (int i = 0; i < servos.length; i++) {
+            //Set the ServoPosition of the servo to be in if it was just in, and out if it was just out
+            servos[i].setPosition(previousStatus == ServoPosition.IN ? positionsOut[i] : positionsIn[i]);
+        }
+        //If the servo was just in, set the flag to be out
+        return (previousStatus == ServoPosition.IN) ? ServoPosition.OUT : ServoPosition.IN;
+    }
+
+    //Flip the servo objects
+    public static ServoPosition flipServos(Servo[] servos, ServoPosition previousStatus) {
+        //For each servo
+        for (int i = 0; i < servos.length; i++) {
+            //Set the ServoPosition of the servo to be in if it was just in, and out if it was just out
+            servos[i].setPosition(previousStatus == ServoPosition.IN ? positionsOutLocal[i] : positionsInLocal[i]);
+        }
+        //If the servo was just in, set the flag to be out
+        return (previousStatus == ServoPosition.IN) ? ServoPosition.OUT : ServoPosition.IN;
+    }
+
+    //Setters for the positions flags (if used)
+    public static void setPositionsOutLocal(double[] positionsOutLocal) {
+        ThreadedServo.positionsOutLocal = positionsOutLocal;
+    }
+
+    public static void setPositionsInLocal(double[] positionsInLocal) {
+        ThreadedServo.positionsInLocal = positionsInLocal;
+    }
+
+    //Move the servos to a specific position
+    public static void moveToPositions(Servo[] servos, double[] positions) {
+        for (int i = 0; i<servos.length; i++) {
+            servos[i].setPosition(positions[i]);
+        }
+    }
+}
