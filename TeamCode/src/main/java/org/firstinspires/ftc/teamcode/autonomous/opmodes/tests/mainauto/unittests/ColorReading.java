f1383dd7e8e86549d679c0ea9a83e7ef23f82c0a
morzack <john_kesler@caryacademy.org>
2018-01-07 18:19:14 -0500

    reenabledautomain and added sensor unit test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/opmodes/tests/mainauto/unittests/ColorReading.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/opmodes/tests/mainauto/unittests/ColorReading.java
@@ -1,12 +1,13 @@
 
-package org.firstinspires.ftc.teamcode.autonomous.opmodes;
+package org.firstinspires.ftc.teamcode.autonomous.opmodes.tests.mainauto.unittests;
 
 import com.qualcomm.hardware.lynx.LynxI2cColorRangeSensor;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.GyroSensor;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
-import com.qualcomm.robotcore.eventloop.opmode.*;
 
 import org.firstinspires.ftc.robotcore.external.navigation.RelicRecoveryVuMark;
 import org.firstinspires.ftc.teamcode.Utils.Robot;
@@ -16,31 +17,16 @@ import org.firstinspires.ftc.teamcode.autonomous.utilities.ThreadedServoMovement
 import org.firstinspires.ftc.teamcode.autonomous.vuforia.VuforiaManager;
 
 @Autonomous(name="Main Autonomous")
-public class AutonomousMain extends LinearOpMode {
+public class ColorReading extends LinearOpMode {
 
     //How long the game has run
     private final ElapsedTime runtime = new ElapsedTime();
 
-    //TODO verify and correct these constants
-    private final double maxTimeVuforia = 5;        //max time (in seconds) to look for a target
-    private double straightPower = 0.75f;           //power when moving
-    private double turnPower = 0.25f;               //when turning
-    private double servoHalfDistance = 0.5f;        //The distance for the jewel servo to be straight out
-    private double servoFullDistance = 1f;          //pivoted towards the jewel sensor
-    private double servoNoDistance = 0f;            //away from the jewel sensor
-
     @Override
     public void runOpMode() {
-        //initialize robot
-        Robot robot = new Robot();
-        robot.init(hardwareMap);
 
         LynxI2cColorRangeSensor jewelColor = hardwareMap.get(LynxI2cColorRangeSensor.class, "Sensor Color Jewel");      //Color sensor onboard jewel arm
         LynxI2cColorRangeSensor boardColor = hardwareMap.get(LynxI2cColorRangeSensor.class, "Sensor Color Ground");     //sensor to read the board
-
-        GyroSensor gyro = hardwareMap.get(GyroSensor.class, "Sensor Gyro");     //Gyro to use when turning
-
-        Servo jewelServo = hardwareMap.get(Servo.class, "Servo Jewel");     //Jewel servo
 
         //Let user know that robot has been initialized
         telemetry.addData("Status", "Core Initialized");
@@ -50,9 +36,6 @@ public class AutonomousMain extends LinearOpMode {
         int colorR = boardColor.red();
         int colorB = boardColor.blue();
         boolean sideColor = colorR>colorB;  //true if red
-
-        //get side of the field that the robot is on
-        boolean sideField = false;  //true if on doublebox side
 
         //Wait For Play, Start Timer
         waitForStart();
