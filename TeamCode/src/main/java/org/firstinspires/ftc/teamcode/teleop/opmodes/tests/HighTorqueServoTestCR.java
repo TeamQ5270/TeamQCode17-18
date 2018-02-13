7087bbaac0a0bce5952fb5a1454385f1a61998cf
spage142edu <spage142edu@gmail.com>
2018-02-13 15:59:25 -0500

    fixed variable problem in high torque servo test opmodes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/opmodes/tests/HighTorqueServoTestCR.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/opmodes/tests/HighTorqueServoTestCR.java
@@ -1,40 +1,41 @@
 package org.firstinspires.ftc.teamcode.teleop.opmodes.tests;
 
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.CRServo;
-import com.qualcomm.robotcore.hardware.Servo;
+
 /**
  * Created by Sheridan_Page on 2/13/2018.
  */
 
-@TeleOp(name = "High Torque Servo Test - Normal", group =  "Linear Opmode")
-public class HighTorqueServoTestNormal extends LinearOpMode{
+@TeleOp(name = "High Torque Servo Test - CR", group =  "Linear Opmode")
+public class HighTorqueServoTestCR extends LinearOpMode{
 
     @Override
     public void runOpMode() {
-        Servo s = null;
+        CRServo s = null;
 
-        s = hardwareMap.servo.get("Servo Relic Rotator");
+        s = hardwareMap.crservo.get("Servo Relic Rotator");
 
         telemetry.addData("Status: ", "Initialized");
         telemetry.update();
 
+        double power = 0.0;
+
         waitForStart();
 
         while (opModeIsActive()) {
-            double position = 0.0;
+
 
             if (gamepad1.y) {
-                position += 0.05;
+                power += 0.05;
             } else if (gamepad1.a) {
-                position -= 0.05;
+                power -= 0.05;
             }
 
-            s.setPosition(position);
+            s.setPower(power);
 
-            telemetry.addData("Servo Position: ", position);
+            telemetry.addData("Servo Power: ", power);
             telemetry.update();
         }
     }
